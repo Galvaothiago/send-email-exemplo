4cf5620c29250ff7135dc93216b1d528eeeac448
Thiago Galvao <thiagosilva128@gmail.com>
2022-03-01 09:59:45 -0300

    created project using Spring Initializr

---
+++ src/main/java/com/exemplo/email/exemplo/EmailExemploApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.email.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailExemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemplo/email/exemplo/EmailExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.email.exemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
